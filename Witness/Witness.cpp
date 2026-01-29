a1c60e7a109db3a6acbc9c40db478ffd5f0fe514
Jarry0926 <sparcjarry@gmail.com>
2026-01-29 23:33:20 +0000

    Finish confict path

--- Witness/Witness.cpp
+++ Witness/Witness.cpp
@@ -7,30 +7,27 @@ Witness::Witness() noexcept
 
 Witness::~Witness() noexcept
 {
-    if (this->map != nullptr) {
-        delete[] this->map;
-        this->map = nullptr;
-    }
+    if (this->map != nullptr) delete[] this->map, this->map = nullptr;
 }
 
 auto Witness::LoadModel(const char* p_FileName) noexcept -> void
 {
     FILE* fd = fopen(p_FileName, "rb+");
-    u8_t ctx[18];
+    u8_t ctx[18]; // read once each 18B into ctx
+    // Generate context if model is an empty file
     i32_t c = fgetc(fd);
     if (c == EOF) {
         for (ctx[0] = TOP; ctx[0] <= LEFT; ++ctx[0]) {
-            this->initContextCenter(fd, ctx);
-            this->initContextBorder(fd, ctx);
-            this->initContextCorner(fd, ctx);
+            this->genContextCenter(fd, ctx);
+            this->genContextBorder(fd, ctx);
+            this->genContextCorner(fd, ctx);
         }
-    }
-    else ungetc(c, fd);
+    } else ungetc(c, fd);
+    // Read model into this->policyPool
     while (fread(ctx, 18, 1, fd)) {
         u32_t i = (static_cast<u32_t>(ctx[0]) << 8) + ctx[1];
-        memcpy(&this->probPool[i][0], &ctx[2], 4 * sizeof(float));
-    }
-    fclose(fd);
+        memcpy(&this->policyPool[i][0], &ctx[2], 4 * sizeof(float));
+    } fclose(fd);
 #if defined(_DEBUG)
     this->PrintModel();
 #endif
@@ -41,16 +38,15 @@ auto Witness::StoreModel(const char* p_FileName) noexcept -> void
     FILE* fd = fopen(p_FileName, "wb");
     for (i32_t i = 1023; i >= 0; --i) {
         u8_t bytes[18] = {static_cast<u8_t>(i >> 8), static_cast<u8_t>(i & 0xff)};
-        memcpy(&bytes[2], &this->probPool[i][0], 4 * sizeof(float));
+        memcpy(&bytes[2], &this->policyPool[i][0], 4 * sizeof(float));
         fwrite(bytes, 18, 1, fd);
-    }
-    fclose(fd);
+    } fclose(fd);
 }
 
 auto Witness::PrintModel() noexcept -> void
 {
     for (u32_t i = 0; i < 1024; ++i) {
-        f32_t* pp = &this->probPool[i][0];
+        f32_t* pp = &this->policyPool[i][0];
         if (pp[0] != 0 || pp[1] != 0 || pp[2] != 0 || pp[3] != 0) {
             printf("%u %u %u %u %u %f %f %f %f\n",
                 i >> 8, i >> 6 & 3, i >> 4 & 3, i >> 2 & 3, i & 3,
@@ -66,27 +62,20 @@ auto Witness::LoadMap(const char* p_FileName) noexcept -> void
     u32_t n; // number of bullets
     fscanf(fd, "%u%u%u", &this->height, &this->width, &n);
     u32_t h = this->height += 3, w = this->width += 3;
-    if (this->map != nullptr) {
-        delete[] this->map;
-        this->map = nullptr;
-    }
+    if (this->map != nullptr) delete[] this->map, this->map = nullptr;
     i32_t* mp = this->map = new i32_t[w * h];
     // Specify borders
-    for (i32_t i = w - 1; i >= 0; --i) {
-        mp[i] = mp[(h - 1) * w + i] = 3;
-    }
-    for (i32_t i = h - 1; i >= 0; --i) {
-        mp[i * w] = mp[i * w + w - 1] = 3;
-    }
+    for (i32_t i = w - 1; i >= 0; --i) mp[i] = mp[(h - 1) * w + i] = 3;
+    for (i32_t i = h - 1; i >= 0; --i) mp[i * w] = mp[i * w + w - 1] = 3;
     // Fill bullets
     while (n--) {
         u32_t x, y, z;
         fscanf(fd, "%u%u%u", &x, &y, &z);
         mp[(y + 2) * w + x + 2] = z;
-    }
-    fclose(fd);
+    } fclose(fd);
     // Initialize the starting context
     this->nodePool[0].ctx = mp[2 * w + 2] << 2 | 0x00c3;
+    this->nodePool[0].vis[2 * w + 2] = 1;
 #if defined(_DEBUG)
     for (u32_t i = 0; i < this->height; ++i) { 
         for (u32_t j = 0; j < this->width; ++j)
@@ -96,42 +85,40 @@ auto Witness::LoadMap(const char* p_FileName) noexcept -> void
 #endif
 }
 
-auto Witness::Learn() noexcept -> void
+auto Witness::Learn() noexcept -> bool
 {
     auto& np = this->nodePool;
     auto& ih = this->_iheap; //auto& ih = this->iheap;
-    auto& mp = this->map;
+    i8_t* mp = this->map;
+    i32_t h = this->height, w = this->width;
     ih.push(0); //
     while (ih.size() > 1) {
-        u32_t this_i = ih.top(); //u32_t this_i = ih[1];
-        node_t& nd = np[this_i];
-        ih.pop(); //this->iheapPop(1);
-        u32_t x = nd.x, y = nd.y;
+        auto [x, y, d, lp, c, ctx, vis] = np[ih.top()]; ih.pop(); //auto [x, y, d, lp, c, ctx] = np[ih[1]]; this->iheapPop(1);
         // Reach a goal state
-        if (x == this->width - 2 && y == this->height - 2) {
-            if (this->isSolved()) {
-                this->updateProb();
-                return;
-            }
-            else continue;
+        if (x == w - 2 && y == h - 2) {
+            bool s = this->isSolved();
+            if (s) this->updatePolicy();
+            return s;
         }
-        // Expand current node 
+        // Expand current node
         for (u32_t i = 0; i < 4; ++i) {
-            i32_t nx = x + DX[i], ny = y + DY[i];
-            u32_t offset = ny * this->width + nx;
-            if (mp[offset] == 3) continue; // skip out of map states
-            u32_t ctx = (GET_DIRECTION_FROM[i] << 8)
-                      | (mp[offset] << 6)
-                      | (mp[offset + 1] << 4)
-                      | (mp[offset + this->width + 1] << 2)
-                      |  mp[offset + this->width];
-            u32_t newDepth = nd.depth + 1;
-            f32_t newLog2Prob = nd.log2Prob + log2f(this->probPool[nd.ctx][i]);
-            f32_t newCost = log2f(static_cast<f32_t>(newDepth)) - newLog2Prob;
+            u32_t nx = x + DX[i], ny = y + DY[i]; // position of next step
+            u32_t j = ny * w + nx; // cached for two dimensional index computation
+            if (mp[j] == 3 || vis[j]) continue; // skip out of map and visited states
+            auto nvis = vis; nvis[j] = 1;
+            // handle visited cases
+            u32_t m[4] = {mp[j], mp[j + 1], mp[j + w + 1], mp[j + w]};
+            if (vis[j - w]) m[0] = m[1] = 3;
+            if (vis[j + 1]) m[1] = m[2] = 3;
+            if (vis[j + w]) m[2] = m[3] = 3;
+            if (vis[j - 1]) m[3] = m[0] = 3;
+            
+            u32_t nctx = FROM[i] << 8 | m[0] << 6 | m[1] << 4 | m[2] << 2 | m[3];
+            f32_t nd = d + 1.0f, nlp = lp + log2f(this->policyPool[ctx][i]), nc = log2f(nd) - nlp;
             ih.push(np.size()); //this->iheapPush(newCost, np.size());
-            np.emplace_back(nx, ny, newDepth, newLog2Prob, newCost, ctx);
+            np.emplace_back(nx, ny, nd, nlp, nc, nctx, std::move(nvis));
         }
-    }
+    } return false;
 }
 
 auto Witness::TrainModel(const char* p_ModelFilePath, const char* p_MapFilePath) noexcept -> void
@@ -142,99 +129,93 @@ auto Witness::TrainModel(const char* p_ModelFilePath, const char* p_MapFilePath)
     this->StoreModel(p_ModelFilePath);
 }
 
-inline auto Witness::initContextCenter(FILE* p_FdModel, u8_t* p_Ctx) noexcept -> void
+#define FOR(X) for (i32_t X = 2; X >= 0; --X)
+
+inline auto Witness::genContextCenter(FILE* fd, u8_t* c) noexcept -> void
 {
-    for (i32_t i = 2; i >= 0; --i) {
-        for (i32_t j = 2; j >= 0; --j) {
-            for (i32_t k = 2; k >= 0; --k) {
-                for (i32_t l = 2; l >= 0; --l) {
-                    p_Ctx[1] = (i << 6) | (j << 4) | (k << 2) | l;
-                    f32_t v = -log2f(3.0f), prob[4] = {v, v, v, v};
-                    prob[p_Ctx[0]] = -INF_F32;
-                    memcpy(&p_Ctx[2], prob, 4 * sizeof(float));
-                    fwrite(p_Ctx, 18, 1, p_FdModel);
-                }
-            }
-        }
+    // fd: File descriptor of the model
+    // c: context as source of fwrite
+    FOR(i) FOR(j) FOR(k) FOR(l) {
+        c[1] = i << 6 | j << 4 | k << 2 | l;
+        f32_t v = -log2f(3.0f), p[4] = {v, v, v, v}; // log_{2}(policy)
+        p[c[0]] = -INF_F32;
+        memcpy(&c[2], p, 4 * sizeof(float));
+        fwrite(c, 18, 1, fd);
     }
 }
 
-inline auto Witness::initContextBorder(FILE* p_FdModel, u8_t* p_Ctx) noexcept -> void
+inline auto Witness::genContextBorder(FILE* fd, u8_t* c) noexcept -> void
 {
+    // fd: File descriptor of the model
+    // c: context as source of fwrite
     constexpr u8_t wall[4] = {0xf0, 0x3c, 0x0f, 0xc3};
     constexpr u8_t off[4][2] = {{2, 0}, {6, 0}, {6, 4}, {4, 2}};
-    for (i32_t k = 3; k >= 0; --k) {
-        if (k == p_Ctx[0]) continue;
-        for (i32_t i = 2; i >= 0; --i) {
-            for (i32_t j = 2; j >= 0; --j) {
-                p_Ctx[1] = (i << off[k][0]) | (j << off[k][1]) | wall[k];
-                f32_t prob[4] = {-1.0f, -1.0f, -1.0f, -1.0f};
-                prob[p_Ctx[0]] = prob[k] = -INF_F32;
-                memcpy(&p_Ctx[2], prob, 4 * sizeof(float));
-                fwrite(p_Ctx, 18, 1, p_FdModel);
-            }
+    for (i32_t k = 3; k >= 0; --k) { // loop through direction against wall
+        if (k == c[0]) continue;
+        FOR(i) FOR(j) {
+            c[1] = i << off[k][0] | j << off[k][1] | wall[k];
+            f32_t p[4] = {-1.0f, -1.0f, -1.0f, -1.0f}; // log_{2}(policy)
+            p[c[0]] = p[k] = -INF_F32;
+            memcpy(&c[2], p, 4 * sizeof(float));
+            fwrite(c, 18, 1, fd);
         }
     }
 }
 
-inline auto Witness::initContextCorner(FILE* p_FdModel, u8_t* p_Ctx) noexcept -> void
+inline auto Witness::genContextCorner(FILE* fd, u8_t* c) noexcept -> void
 {
+    // fd: File descriptor of the model
+    // c: context as source of fwrite
     constexpr u8_t wall[4] = {0x3f, 0xcf, 0xf3, 0xfc};
     constexpr u8_t from[4][2] = {{1, 2}, {2, 3}, {0, 3}, {0, 1}};
-    for (i32_t k = 3; k >= 0; --k) {
-        if (from[k][0] == p_Ctx[0] || from[k][1] == p_Ctx[0]) continue;
-        for (i32_t i = 2; i >= 0; --i) {
-            p_Ctx[1] = i << (3 - k << 1) | wall[k];
-            f32_t prob[4] = {0.0f, 0.0f, 0.0f, 0.0f};
-            prob[p_Ctx[0]] = prob[from[k][0]] = prob[from[k][1]] = -INF_F32;
-            memcpy(&p_Ctx[2], prob, 4 * sizeof(float));
-            fwrite(p_Ctx, 18, 1, p_FdModel);
+    for (i32_t k = 3; k >= 0; --k) { // loop through available cell
+        if (from[k][0] == c[0] || from[k][1] == c[0]) continue;
+        FOR(i) {
+            c[1] = i << (3 - k << 1) | wall[k];
+            f32_t p[4] = {0.0f, 0.0f, 0.0f, 0.0f}; // log_{2}(policy)
+            p[c[0]] = p[from[k][0]] = p[from[k][1]] = -INF_F32;
+            memcpy(&c[2], p, 4 * sizeof(float));
+            fwrite(c, 18, 1, fd);
         }
     }
 }
+
+#undef FOR
 
 inline auto Witness::isSolved() noexcept -> bool
 {
     return true;
 }
 
-inline auto Witness::updateProb() noexcept -> void
+inline auto Witness::updatePolicy() noexcept -> void
 {
     
 }
 
-inline auto Witness::iheapPush(f32_t p_Cost, u32_t p_Target_i) noexcept -> void
+inline auto Witness::iheapPush(f32_t v, u32_t i) noexcept -> void
 {
     auto& np = this->nodePool;
     auto& ih = this->iheap;
-    u32_t i = ih.size();
-    ih.push_back(p_Target_i);
-    while (i != 1) {
-        u32_t pi = i >> 1;
-        if (np[ih[pi]].cost > p_Cost) {
-            ih[i] = ih[pi];
-            i = pi;
-        }
+    u32_t j = ih.size(); ih.push_back(i);
+    while (j != 1) {
+        u32_t pi = j >> 1;
+        if (np[ih[pi]].cost > v) ih[j] = ih[pi], j = pi;
         else break;
-    }
-    ih[i] = p_Target_i;
+    } ih[j] = i;
 }
 
-inline auto Witness::iheapPop(u32_t p_Top_i) noexcept -> void
+inline auto Witness::iheapPop(u32_t i) noexcept -> void
 {
     auto& np = this->nodePool;
     auto& ih = this->iheap;
-    u32_t last_v = ih.back();
+    u32_t v = ih.back(); // value in the last node of heap
     ih.pop_back();
-    u32_t lastNonLeaf_i = ih.size() << 1;
-    node_t& nd = np[last_v];
-    while (p_Top_i <= lastNonLeaf_i) {
-        u32_t l = p_Top_i << 1, r = l | 1;
-        np[ih[l]].cost < np[ih[r]].cost ? p_Top_i = l : p_Top_i = r;
-        if (nd.cost > np[ih[p_Top_i]].cost) {
-            ih[p_Top_i >> 1] = ih[p_Top_i];
-        }
+    u32_t j = ih.size() << 1; // index of last non-leaf node in heap
+    f32_t c = np[v].cost;
+    while (i <= j) {
+        u32_t l = i << 1, r = l | 1;
+        np[ih[l]].cost < np[ih[r]].cost ? i = l : i = r;
+        if (c > np[ih[i]].cost) ih[i >> 1] = ih[i];
         else break;
-    }
-    ih[p_Top_i] = last_v;
+    } ih[i] = v;
 }
--- Witness/Witness.h
+++ Witness/Witness.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <bitset>
 #include <cmath>
 #include <cstdint>
 #include <cstdio>
@@ -15,6 +16,7 @@ using u8_t = uint8_t;
 using u16_t = uint16_t;
 using u32_t = uint32_t;
 using f32_t = float;
+using f64_t = double;
 
 class Witness
 {
@@ -31,7 +33,7 @@ public:
 
     auto LoadMap(const char* p_FileName) noexcept -> void;
     
-    auto Learn() noexcept -> void;
+    auto Learn() noexcept -> bool;
 
     auto TrainModel(const char* p_ModelFilePath, const char* p_MapFilePath) noexcept -> void;
     
@@ -40,39 +42,41 @@ private:
     static constexpr i32_t DX[4] = {0, 1, 0, -1};
     static constexpr i32_t DY[4] = {-1, 0, 1, 0};
     static constexpr i32_t TOP = 0, RIGHT = 1, BOTTEM = 2, LEFT = 3;
-    static constexpr u8_t GET_DIRECTION_FROM[4] = {BOTTEM, LEFT, TOP, RIGHT};
+    static constexpr u8_t FROM[4] = {BOTTEM, LEFT, TOP, RIGHT};
     
     struct node_t
     {
         i32_t x, y;
-        u32_t depth;
-        f32_t log2Prob; // policy in log_2 space
+        f32_t depth;
+        f32_t log2policy; // policy in log_2 space
         f32_t cost;
         u32_t ctx; // from direction, bullet states
+        // TODO: optimize it with HLD
+        std::bitset<49> vis; // positions visited in previous search of this branch
     };
     
 private:
-    inline auto initContextCenter(FILE* p_FdModel, u8_t* p_Ctx) noexcept -> void;
+    inline auto genContextCenter(FILE* fd, u8_t* c) noexcept -> void;
 
-    inline auto initContextBorder(FILE* p_FdModel, u8_t* p_Ctx) noexcept -> void;
+    inline auto genContextBorder(FILE* fd, u8_t* c) noexcept -> void;
 
-    inline auto initContextCorner(FILE* p_FdModel, u8_t* p_Ctx) noexcept -> void;
+    inline auto genContextCorner(FILE* fd, u8_t* c) noexcept -> void;
 
     // [Called after reaching a goal state]
     inline auto isSolved() noexcept -> bool;
 
     // [Called after this->isSolved() returns true]
-    // Trace every context and update probabilities 
-    inline auto updateProb() noexcept -> void;
+    // Trace every context and update policyabilities 
+    inline auto updatePolicy() noexcept -> void;
     
-    inline auto iheapPush(f32_t p_Cost, u32_t p_Target_i) noexcept -> void;
+    inline auto iheapPush(f32_t v, u32_t i) noexcept -> void;
     
-    inline auto iheapPop(u32_t p_Top_i) noexcept -> void;
+    inline auto iheapPop(u32_t i) noexcept -> void;
 
     u32_t height, width;
     i32_t* map; // a cell corresponds to a junction at its bottom-right
-    f32_t probPool[1024][4] = {};
-    std::vector<node_t> nodePool = {{2, 2, 1, 0.0f, 0}};
+    f32_t policyPool[1024][4] = {};
+    std::vector<node_t> nodePool = {{2, 2, 1.0f, 0.0f, 0, 2}};
     std::vector<u32_t> iheap = {0, 0};
     std::priority_queue<u32_t> _iheap;
 
